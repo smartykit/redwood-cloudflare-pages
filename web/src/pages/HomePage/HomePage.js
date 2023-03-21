0dc92d5980edd67311527e0e7d6f66a92fd8ee9e
Sergei Panarin <sergpanarin@gmail.com>
2023-03-21 23:04:50 +0100

    Initial commit to deploy

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Redwood JS & Cloudflare Pages 🚀</h1>
+      <p>Woot!</p>
+    </>
+  )
+}
+
+export default HomePage
